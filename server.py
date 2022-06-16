1f5222e92d24982d22772666bcc644e6d4e13a74
David Perelroizen <david.perelroizen@gmail.com>
2022-06-16 09:44:57 +0200

    Added correction so that flask server could be launched

--- server.py
+++ server.py
@@ -57,3 +57,8 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+
+# Correction 1: code below was missing --> server couldn't be launched
+if __name__ == "__main__":
+    app.run()
