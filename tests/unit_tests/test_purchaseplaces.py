87d760e4c565d36069cd6d585aa4b733f27ecc02
David Perelroizen <david.perelroizen@gmail.com>
2022-06-22 22:42:29 +0200

    purchasePlaces() test attempt. Created a conftest.py file to set-up the client fixture

---
+++ tests/unit_tests/test_purchaseplaces.py
@@ -0,0 +1,51 @@
+import pytest
+from flask import Flask,render_template,request,redirect,flash,url_for
+
+clubs = [
+        {
+            "name": "Simply Lift",
+            "email": "john@simplylift.co",
+            "points": "13"
+        },
+        {
+            "name": "Iron Temple",
+            "email": "admin@irontemple.com",
+            "points": "4"
+        },
+        {
+            "name": "She Lifts",
+            "email": "kate@shelifts.co.uk",
+            "points": "12"
+        }]
+
+competitions: [
+        {
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Fall Classic",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13"
+        }
+    ]
+
+
+form_data = {'competition': 'Spring Festival', 'places': 4, 'club': 'Iron Temple'}
+
+
+def test_buying_places_should_decrease_points_available(client):
+    response = client.post('/purchasePlaces', json=form_data)
+
+    assert clubs[1]["points"] == 0
+
+
+
+
+def test_club_cant_buy_more_places_than_points_available():
+    pass
+
+
+def test_club_cant_buy_more_than_12_places():
+    pass
