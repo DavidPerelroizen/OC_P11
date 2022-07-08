cbc5ffcaad21e1d906d3ac30e332b1e3dfdf61cd
David Perelroizen <david.perelroizen@gmail.com>
2022-07-08 17:20:17 +0200

    Added wait time

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,13 +1,18 @@
-from locust import HttpUser, task
+import time
+
+from locust import HttpUser, task, between
 
 
 class QuickStartUser(HttpUser):
+    wait_time = between(1, 5)
 
     @task
     def show_summary(self):
-        self.client.post('/showSummary', json={'email': 'admin@irontemple.com'})
+        self.client.post('/showSummary', data={'email': 'admin@irontemple.com'})
+        time.sleep(1)
 
     @task
     def points_total_update(self):
-        self.client.post('/purchasePlaces', json={'name': 'Iron Temple', 'competition': 'Spring Festival',
+        self.client.post('/purchasePlaces', data={'name': 'Iron Temple', 'competition': 'Spring Festival',
                                                   'places': 4})
+        time.sleep(1)
