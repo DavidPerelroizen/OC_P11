a0ba568b47a796d4ceb6b3e49b6cbde4fcd8c424
David Perelroizen <david.perelroizen@gmail.com>
2022-07-11 11:26:16 +0200

    Added tests for book page

--- tests/unit_tests/test_book.py
+++ tests/unit_tests/test_book.py
@@ -3,36 +3,52 @@ import pytest
 from server import book, clubs, competitions
 from flask import render_template
 from .conftest import client
+from bs4 import BeautifulSoup
 
-"""
-def test_booking_with_wrong_competition_and_right_club():
-    competition = "Spring Festival"
+
+def test_booking_with_wrong_competition_and_right_club(client):
+    competition = "Spring wrongFestival"
 
     club = "Simply Lift"
 
-    with pytest.raises(IndexError):
-        book(competition, club)
+    assertion_check = 'Something went wrong-please try again'
+
+    response = client.get('/book/' + competition + '/' + club)
+
+    soup = BeautifulSoup(response.data, features="html.parser")
+    soup_content = soup.find_all("li")
+
+    assert assertion_check in soup_content[0].get_text()
 
 
-def test_booking_with_right_competition_and_wrong_club():
+def test_booking_with_right_competition_and_wrong_club(client):
     competition = "Spring Festival"
 
     club = "Simply WrongLift"
 
-    with pytest.raises(IndexError):
-        book(competition, club)
+    assertion_check = 'Something went wrong-please try again'
 
+    response = client.get('/book/' + competition + '/' + club)
 
-def test_booking_error_message():
+    soup = BeautifulSoup(response.data, features="html.parser")
+    soup_content = soup.find_all("li")
+
+    assert assertion_check in soup_content[0].get_text()
+
+def test_booking_error_message(client):
     competition = "Spring Festival"
-
     club = "Simply WrongLift"
+    assertion_check = 'Something went wrong-please try again'
 
-    with pytest.raises(IndexError):
-        book(competition, club)
-"""
+    response = client.get('/book/' + competition + '/' + club)
 
-def test_booking_route():
+    soup = BeautifulSoup(response.data, features="html.parser")
+    soup_content = soup.find_all("li")
+
+    assert assertion_check in soup_content[0].get_text()
+
+
+def test_booking_route(client):
     competition = "Spring Festival"
 
     club = "Simply WrongLift"
