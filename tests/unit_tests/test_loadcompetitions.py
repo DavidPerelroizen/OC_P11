9e6ddc79ccc3dd74d95a78a0e6b29cb0909fb943
David Perelroizen <david.perelroizen@gmail.com>
2022-07-11 12:42:17 +0200

    Modified test

--- tests/unit_tests/test_loadcompetitions.py
+++ tests/unit_tests/test_loadcompetitions.py
@@ -5,12 +5,12 @@ def test_loadcompetitions_should_return_competitions_list():
     expected_value = [
         {
             "name": "Spring Festival",
-            "date": "2020-03-27 10:00:00",
+            "date": "2022-07-27 10:00:00",
             "numberOfPlaces": "25"
         },
         {
             "name": "Fall Classic",
-            "date": "2020-10-22 13:30:00",
+            "date": "2022-10-22 13:30:00",
             "numberOfPlaces": "13"
         }
     ]
