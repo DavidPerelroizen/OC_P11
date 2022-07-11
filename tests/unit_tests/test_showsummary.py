adf1c69829667c603f370e1d67e52b14337f55e2
David Perelroizen <david.perelroizen@gmail.com>
2022-07-11 11:55:13 +0200

    Improved showsummary tests

--- tests/unit_tests/test_showsummary.py
+++ tests/unit_tests/test_showsummary.py
@@ -1,52 +1,47 @@
 from server import showSummary
+from bs4 import BeautifulSoup
+
+clubs = [
+    {
+        "name": "Simply Lift",
+        "email": "john@simplylift.co",
+        "points": "13"
+    },
+    {
+        "name": "Iron Temple",
+        "email": "admin@irontemple.com",
+        "points": "4"
+    },
+    {
+        "name": "She Lifts",
+        "email": "kate@shelifts.co.uk",
+        "points": "12"
+    }]
 
 
-def test_showsummary_with_good_email():
-    email_fortest = "admin@irontemple.com"
-    clubs = [
-        {
-            "name": "Simply Lift",
-            "email": "john@simplylift.co",
-            "points": "13"
-        },
-        {
-            "name": "Iron Temple",
-            "email": "admin@irontemple.com",
-            "points": "4"
-        },
-        {
-            "name": "She Lifts",
-            "email": "kate@shelifts.co.uk",
-            "points": "12"
-        }]
+def test_showsummary_with_good_email(client):
+    form_data = {'email': 'admin@irontemple.com'}
 
-    club = [club for club in clubs if club['email'] == email_fortest][0]
+    assertion_check = 'Welcome, admin@irontemple.com'
 
-    assert club == clubs[1]
+    response = client.post('/showSummary', data=form_data)
+
+    soup = BeautifulSoup(response.data, features="html.parser")
+    soup_content = soup.find_all("h2")
+
+    assert assertion_check in soup_content[0].get_text()
 
 
-def test_showsummary_with_wrong_email():
-    email_fortest = "test@test.com"
-    clubs = [
-        {
-            "name": "Simply Lift",
-            "email": "john@simplylift.co",
-            "points": "13"
-        },
-        {
-            "name": "Iron Temple",
-            "email": "admin@irontemple.com",
-            "points": "4"
-        },
-        {
-            "name": "She Lifts",
-            "email": "kate@shelifts.co.uk",
-            "points": "12"
-        }]
-    try:
-        club = [club for club in clubs if club['email'] == email_fortest][0]
-        assert club == clubs[1]
-    except IndexError:
-        assert 1 == 1
+def test_showsummary_with_wrong_email(client):
+    form_data = {'email': 'test@test.com'}
+
+    assertion_check = 'You should be redirected automatically to the target URL: '
+
+    response = client.post('/showSummary', data=form_data)
+
+    soup = BeautifulSoup(response.data, features="html.parser")
+    soup_content = soup.find_all("p")
+
+    assert assertion_check in soup_content[0].get_text()
 
 
